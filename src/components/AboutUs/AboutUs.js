758487293846293d8d68449786f974c5bafd4bc5
YuraKolyada <yyoorraa@gmail.com>
2017-06-17 19:04:16 +0300

    added for aboutUs innerHTML for content text

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -6,6 +6,7 @@ import {connect} from 'react-redux';
 import s from "./AboutUs.scss";
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import Link from '../Link/Link';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 class AboutUs extends React.Component {
@@ -20,8 +21,6 @@ class AboutUs extends React.Component {
 
     render() {
         let { title, content, img } = this.props.AboutData;
-        let allText = content.replace(/<p>/g, ''),
-            text = allText.split('</p>');
         return (
             <div className={s.root}>
                 <div className={s.container}>
@@ -30,11 +29,7 @@ class AboutUs extends React.Component {
                     </div>
                     <div className={s.topic}>
                         <h2 className={ this.props.bgPadding ? s.bgTitle : s.title}>{title}</h2>
-                        <div className={s.text}>
-                        { text.map((str, index)=> 
-                            <p className={s.str} key={index}>{str}</p>
-                        )}
-                        </div>
+                        <div className={s.text} dangerouslySetInnerHTML={{__html: content}}></div>
                         <div className={s.btn}>
                             <NumberList className={s.list} numbers={numbers} />
                         </div>
